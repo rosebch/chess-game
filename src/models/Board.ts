90d03d0c83ba94d2514365b22739302438db923e
Rose <rosebatoeva@gmail.com>
2022-07-08 20:42:51 +0300

    added chess figures

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -17,4 +17,12 @@ export class Board {
             this.cells.push(row) //Пушим ряды в cells
         }
     }
+
+    public getCell(x: number, y: number) {
+        return this.cells[y][x]
+    }
+
+    public addFigures() {
+       
+    }
 }
