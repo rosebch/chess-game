a6fc93d3b49240d0e86e129bdfc1abe88328cd24
Rose <rosebatoeva@gmail.com>
2022-07-13 13:39:10 +0300

    added Timer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Board } from './models/Board';
 import {Player} from "./models/Player";
 import {Colors} from "./models/Colors";
 import LostFigures from "./components/LostFigures";
+import Timer from "./components/Timer";
 
 const App = () => {
   const [board, setBoard] = useState(new Board())
@@ -29,7 +30,11 @@ const App = () => {
   }
 
   return (
-    <div className="pp">
+    <div className="App">
+      <Timer
+        restart={restart}
+        currentPlayer={currentPlayer}
+      />
       <BoardComponent 
         board={board}
         setBoard={setBoard}
